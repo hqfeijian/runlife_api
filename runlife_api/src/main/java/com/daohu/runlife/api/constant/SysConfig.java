91341c404a8d68e91507e0afd06ad03d010cdaa4
hqfeijian <1661044722@qq.com>
2018-06-14 11:04:20 +0800

    优化兑换步数算法;增加部署合约密码
    优化兑换步数算法，按照每总量剩余一半的时候兑换难度增加一倍，如目前按照平均步数5112计算，总量14亿，每天5112步可以兑换1step，当剩下不足7亿时，5112*2步数才可以兑换1step，依次类推

--- runlife_api/src/main/java/com/daohu/runlife/api/constant/SysConfig.java
+++ runlife_api/src/main/java/com/daohu/runlife/api/constant/SysConfig.java
@@ -4,4 +4,6 @@ public class SysConfig {
     public final static String walletSalt = "***";
     public final static String wxMiniAppid = "wxMiniAppid";
     public final static String wxMiniAppSecret = "wxMiniAppSecret";
+	public final static String adminDeployPassword = "adminDeployPassword";
+	
 }
